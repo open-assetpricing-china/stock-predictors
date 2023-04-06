6754349fa056b4f740ed423d1349a350e1fe586c
yangyuan16_glb <50294636+yangyuan16@users.noreply.github.com>
2023-04-06 22:31:23 +0800

    upgrade

--- codes/predictors/acc.py
+++ codes/predictors/acc.py
@@ -6,7 +6,7 @@
 # CA:   'A001100000', current assets
 # CASH: 'A001101000', cash / cash equivalents
 # CL:   'A002100000', current liabilities
-# STD:  'A002126000', debt included in current liabilities
+# STD:  'A002125000', Non-current Liabilities Due within One Year
 # TP:   'B002100000', income tax payable
 # Dep:  'D000103000', depreciation expense 折旧费用
 # Dep:  'D000104000', amortization expense 摊销费用
@@ -16,7 +16,7 @@
 def equation(x):
     x = x.copy()
     x['acc'] =( (x['A001100000'].diff() - x['A001101000'].diff()) - (
-            x['A002100000'].diff() - x['A002126000'].diff() - x['B002100000'].diff()) - (
+            x['A002100000'].diff() - x['A002125000'].diff() - x['B002100000'].diff()) - (
             x['D000103000'] + x['D000104000']) ) / x['A001000000']
     return x
 def lag_one_month(x):
@@ -25,7 +25,7 @@ def lag_one_month(x):
     return x
 def calculation(df_input):
     df = df_input['monthly']
-    df_output = df[['stkcd', 'month', 'A001100000','A001101000','A002100000','A002126000',
+    df_output = df[['stkcd', 'month', 'A001100000','A001101000','A002100000','A002125000',
                     'B002100000','D000103000','D000104000','A001000000']]
     df_output = df_output.groupby('stkcd').apply(lambda x: equation(x)).reset_index(drop=True)
     df_output = df_output[['stkcd', 'month', 'acc']]
--- codes/predictors/pctacc.py
+++ codes/predictors/pctacc.py
@@ -8,7 +8,7 @@
 # CA:   'A001100000', current assets
 # CASH: 'A001101000', cash / cash equivalents
 # CL:   'A002100000', current liabilities
-# STD:  'A002126000', debt included in current liabilities
+# STD:  'A002125000', Non-current Liabilities Due within One Year
 # TP:   'B002100000', income tax payable
 # Dep:  'D000103000', depreciation expense 折旧费用
 # Dep:  'D000104000', amortization expense 摊销费用
@@ -18,7 +18,7 @@
 def equation(df):
     df = df.copy()
     df['pctacc'] =(( (df['A001100000'].diff(periods=3) - df['A001101000'].diff(periods=3)) - (
-        df['A002100000'].diff(periods=3) - df['A002126000'].diff(periods=3) -
+        df['A002100000'].diff(periods=3) - df['A002125000'].diff(periods=3) -
         df['B002100000'].diff(periods=3)) - (df['D000103000']
                                              + df['D000104000']) ).abs() / df['A001000000']).pct_change(periods=3)
     return df
@@ -30,7 +30,7 @@ def lag_one_month(x):
 #
 def calculation(df_input):
     df_output = df_input['monthly'][['stkcd', 'month', 'A001100000','A001101000','A002100000',
-                                     'A002126000', 'B002100000','D000103000',
+                                     'A002125000', 'B002100000','D000103000',
                                      'D000104000','A001000000']]
     df_output = df_output.groupby('stkcd').apply(equation).reset_index(drop=True)
     df_output = df_output[['stkcd', 'month', 'pctacc']]
